eb43b96d62eebe8283166edd39853b3bca961046
ZhannaKarpova <92864174+ZhannaKarpova@users.noreply.github.com>
2021-11-16 20:59:41 +0700

    Добавьте файлы проекта.

---
+++ Lab_8_2_10.11/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Lab_8_2_10._11
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = File.CreateText("F1.txt"); 
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(rnd.Next(100)); 
+                }
+            }
+            sw.Close(); 
+
+            using (StreamReader sr = new StreamReader("F1.txt"))
+            {
+                string numbers = sr.ReadLine();
+                foreach (var number in numbers.Split())
+                {
+                    Console.WriteLine(number);
+                    int x = int.Parse(Console.ReadLine());
+                    int sum = 0;
+                    for (int i = 10; i >= 0; i--)
+                    {
+                        sum += i;
+
+                    }
+                    Console.WriteLine(sum);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}  
+
